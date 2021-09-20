13b7d95909f79c360b918869452d7c3d8b37665a
the-fukui <account@the-fukui.com>
2021-09-20 23:05:42 +0900

    add hasura

--- env.sh
+++ env.sh
@@ -3,6 +3,6 @@
 # gcloud config set project [project-id] && \
 # gcloud secrets versions access latest --secret [secret-name] >.env.local && \
 ls -a .env* | xargs -I {} ln -sf ../../{} src/shared/{} && \
-ls -a .env* | xargs -I {} ln -sf ../../{} src/graphql/{} && \
+ls -a .env* | xargs -I {} ln -sf ../../{} src/hasura/{} && \
 ls -a .env* | xargs -I {} ln -sf ../../{} src/web/{}
 # ls -a .env* | xargs -I {} ln -sf ../../../{} src/firebase/functions/{} && \
