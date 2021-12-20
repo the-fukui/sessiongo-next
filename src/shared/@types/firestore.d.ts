8085018c3caf88fed6ba3e5919355ad2cda1175d
the-fukui <account@the-fukui.com>
2021-12-20 22:14:56 +0900

    session, place type

--- src/shared/@types/firestore.d.ts
+++ src/shared/@types/firestore.d.ts
@@ -1,5 +1,29 @@
+import { SessionFreqType } from './firestore.d'
 export interface User {
   displayName: string | null
   createdAt: number
   updatedAt: number
+}
+
+export type SessionStatus = 'public' | 'draft' | 'protected'
+
+/**
+ * 繰り返しイベントは
+ * @see https://github.com/jakubroztocil/rrule
+ * @see https://tex2e.github.io/rfc-translater/html/rfc5545.html
+ */
+export interface Session {
+  createdAt: number
+  updatedAt: number
+  title: string | null
+  description: string | null
+  status: SessionStatus
+  startAt: number
+  endAt: number
+  rrule: string | null
+  placeID: string //google map API placeID
+}
+
+export interface Place {
+  name: string
 }
